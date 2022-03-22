94839302233ff411c9b8a8a049124031ba130171
Henry Chen <henrychennj@gmail.com>
2022-03-21 23:28:01 -0400

    Changed App to toggle render on click

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import SearchBar from './Search';
 
 
 function App() {
+  const [renderWin, setRenderWin] = useState("Courses");
+
+  useEffect(() =>{
+    console.log("TEST")
+  });
+  
+  if(renderWin == "Courses"){
+    return buildCoursesPage(setRenderWin);
+  }
+  else{
+    return buildSchedulesPage(setRenderWin);
+  }
+
+}
+
+function buildCoursesPage(setRenderWin : Function)
+{
   return (
     <div className="App">
       <header className="App-header">
 
+        <div className="ToggleSchdl">
+          <button onClick = {() => setRenderWin("SampSchdl")}>See Sample Schedules</button> 
+        </div>
+        
         <div className="listCourses">
           List of Courses
           <SearchBar/>
@@ -21,6 +42,17 @@ function App() {
         </div>
 
       </header>
+    </div>
+  );
+}
+
+function buildSchedulesPage(setRenderWin : Function)
+{
+  return(
+    <div className="App">
+      <div className="ToggleCourses">
+            <button onClick = {() => setRenderWin("Courses")}>See Courses</button> 
+      </div>
     </div>
   );
 }
