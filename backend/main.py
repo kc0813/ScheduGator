ffa6444d9309ec18804019f3446c4f660f0b47e0
kc0813 <katherinechan4@gmail.com>
2022-03-22 00:56:42 -0400

    get a boilerplate up for fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,59 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
+import requests
+from models import ClassQuery
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
+    """
+    Default endpoint
+    Currently set to message back hello world
+    """
     return {"message": "Hello World"}
+
+
+@app.put("/class/")
+async def queryClass(
+    query: ClassQuery = Body(
+        ...,
+        examples={
+            "invalid": {
+                "summary": "Query description",
+                "description": "Description of the parameters, **DO NOT** query this.",
+                "value": {
+                    "category": "CWSP for on campus students, IA for innovation academy students",
+                    "term": "2221, see rolstenhouse api for more detail",
+                    "isQuest": "True if you want to see quest 1 classes",
+                    "genEd": "The genEd you want to see",
+                    "writing": "writing requirement, 2000, 4000, or 6000",
+                    "meeting": "The meeting days you want to see M, T, W, R, F",
+                },
+            },
+            "normal": {
+                "summary": "Example query",
+                "description": "Example that you can **actually** query",
+                "value": {
+                    "category": "CWSP",
+                    "term": 2221,
+                    "isQuest": False,
+                    "genEd": None,
+                    "writing": 2000,
+                    "meeting": "R",
+                },
+            },
+        },
+    ),
+):
+    """
+    Query class information
+    """
+    # setup parameters to query uf soc
+    url = "https://one.uf.edu/apix/soc/schedule"
+    params = {
+        "category": query.category,
+        "term": query.term,
+    }
+    r = requests.get(url=url, params=params)
+    return r.json()
---
+++ backend/models.py
@@ -0,0 +1,33 @@
+from unicodedata import category
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+
+
+class ClassQuery(BaseModel):
+    """
+    ClassQuery is the model for looking up classes
+    """
+
+    # API requires this to query properly
+    category: str = "CWSP"
+    term: int = 2221
+    # priority 1
+    # quest will be quest 1
+    isQuest: bool | None
+    genEd: str | None
+    writing: int | None
+    # TODO: specify R for thursday
+    meeting: str | None
+
+    # TODO see https://fastapi.tiangolo.com/tutorial/schema-extra-example/
+    class Config:
+        schema_extra = {
+            "example": {
+                "category": "CWSP",
+                "term": 2221,
+                "isQuest": False,
+                "genEd": None,
+                "writing": 2000,
+                "meeting": "MWF",
+            }
+        }
