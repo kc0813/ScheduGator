5ce9332603056e34814e08a33bf1f3bf05b9df86
pixelfac <pixelfac.nerfbros@gmail.com>
2022-03-21 22:09:49 -0400

    purify index.tsx
    purge index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,59 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-let renderVar = {
-  toRender: "App",
-  nextRender: "CourseSearch",
-}
-
-function changeRenderState(start: boolean){
-  if(start) {
-    renderVar.toRender = "CourseSearch";
-  }
-  
-  if(renderVar.toRender === "App")
-  {
-    ReactDOM.render(
-      <div>
-        <button onClick = {() => changeRenderState(false)}> See Schedule </button>
-        <App/> 
-      </div>,
-    document.getElementById('root')
-    );
-    renderVar.toRender = "CourseSearch";
-    
-  }
-  else if(renderVar.toRender === "CourseSearch")
-  {
-    ReactDOM.render(
-      <div>
-        <button onClick = {() => changeRenderState(false)}> Back to Course Search </button>
-      </div>,
-    document.getElementById('root')
-    );
-    renderVar.toRender = "App";
-  }
-}
 
 //Render multiple components by just adding them to the array.
 ReactDOM.render(
-  <div>
-    <button onClick = {() => changeRenderState(true)}> See Schedule </button>
-    <App/>
-  </div>,
-  document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 );
 
-/*
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
-*/
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
