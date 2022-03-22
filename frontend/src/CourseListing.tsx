a44ea7da9b83056c4d7abf8f8a23df0b07e8a711
pixelfac <pixelfac.nerfbros@gmail.com>
2022-03-22 00:01:04 -0400

    spaces=4

--- frontend/src/CourseListing.tsx
+++ frontend/src/CourseListing.tsx
@@ -5,29 +5,29 @@ function CourseListing(setRenderWin : Function)
 {
     return (
         <div className="App">
-        <header className="App-header">
+            <header className="App-header">
 
-            <div className="ToggleSchdl">
-                <button
-                    onClick = {() => setRenderWin("SampSchdl")}
-                >
-                    See Sample Schedules
-                </button> 
-            </div>
-            
-            <div className="listCourses">
-                List of Courses
-                <SearchBar/>
-            </div>
+                <div className="ToggleSchdl">
+                    <button
+                        onClick = {() => setRenderWin("SampSchdl")}
+                    >
+                        See Sample Schedules
+                    </button> 
+                </div>
+                
+                <div className="listCourses">
+                    List of Courses
+                    <SearchBar/>
+                </div>
 
-            <div className="searchOptions">
-                Search Options
-            </div>
+                <div className="searchOptions">
+                    Search Options
+                </div>
 
-            <div className="courses">
-            </div>
+                <div className="courses">
+                </div>
 
-        </header>
+            </header>
         </div>
     );
 }
--- frontend/src/Schedule.tsx
+++ frontend/src/Schedule.tsx
@@ -1,12 +1,12 @@
 function Schedule(setRenderWin : Function)
 {
-  return(
+	return(
     <div className="App">
         <div className="ToggleCourses">
             <button onClick = {() => setRenderWin("Courses")}>See Courses</button> 
         </div>
     </div>
-  );
+	);
 }
 
 export default Schedule
