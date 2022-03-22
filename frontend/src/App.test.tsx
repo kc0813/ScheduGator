c62c3283f096077fb714bee4ca9b513256bbb72a
pixelfac <pixelfac.nerfbros@gmail.com>
2022-03-22 13:57:15 -0400

    write first proper test for App

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { Container, render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+let container: any = null;
+
+
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+
+describe(App, () => {
+    it('loads default page', () => {
+        act(() => {
+            render(<App/>, container)
+        })
+        expect(container.firstChild.className).toBe('CourseListing')
+    })
+})
