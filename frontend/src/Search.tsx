8124a0b13855102df2c41a26fe45a66265bd61d6
helarious99 <helenahe@ufl.edu>
2022-03-24 00:39:59 -0400

    modified search bar to get input

--- frontend/src/Search.tsx
+++ frontend/src/Search.tsx
@@ -1,3 +1,12 @@
+const getInputValue = (event: any)=>{
+    // do cool stuff here
+    if (event.key === 'Enter') {
+        const userValue = event.target.value;
+        console.log(userValue);
+        event.preventDefault();  // Stop page from refreshing after pressing enter
+      }
+};
+
 const SearchBar = () => (
     <form action="/" method="get" className = "SearchBar">
         <label htmlFor="header-search">
@@ -8,6 +17,7 @@ const SearchBar = () => (
             id="header-search"
             placeholder="Search Courses"
             name="s" 
+            onKeyPress={getInputValue}
         />
     </form>
   );
