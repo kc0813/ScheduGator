a21853dcc57cbff65b99983bc4697c2b321f697c
pixel <43653484+pixelfac@users.noreply.github.com>
2022-03-25 15:05:30 -0500

    add shallow dropdown to search options

--- frontend/src/CourseListing/SearchOptions.tsx
+++ frontend/src/CourseListing/SearchOptions.tsx
@@ -1,8 +1,11 @@
 const SearchOptions = () => {
     return <div id='SearchOptions'>
-        <button>
-            Class ID
-        </button>
+        <select>
+            <option value=''>--Word Count--</option>
+            <option value='2000'>2000 words</option>
+            <option value='4000'>4000 words</option>
+            <option value='6000'>6000 words</option>
+        </select>
     </div>
 }
 
