5a5689eaf2b9fe390804a18854d4a2cc8989e774
pixelfac <pixelfac.nerfbros@gmail.com>
2022-03-28 16:22:47 -0400

    add form tag to word count

--- frontend/src/CourseListing/SearchOptions/WordCount.tsx
+++ frontend/src/CourseListing/SearchOptions/WordCount.tsx
@@ -6,15 +6,15 @@ const WordCount = () => {
         setWordCount(event.target.value)
     }
 
-    return <div>
-        Word Count<br/>
+    return <form>
+        <label>Word Count</label><br/>
         <select id='WordCount' onChange={updateHandler}>
             <option value=''>--</option>
             <option value='2000'>2000 words</option>
             <option value='4000'>4000 words</option>
             <option value='6000'>6000 words</option>
         </select>
-    </div>
+    </form>
 }
 
 
