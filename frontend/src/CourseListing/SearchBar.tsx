5b58317844102aa46bc5d9722a0b8d4972e9950b
Henry Chen <henrychennj@gmail.com>
2022-03-28 20:04:48 -0400

    input validation for CourseID

--- frontend/src/CourseListing/SearchBar.tsx
+++ frontend/src/CourseListing/SearchBar.tsx
@@ -9,13 +9,50 @@ function SearchBar(props: {AddCourse: (courseID: string) => void}) {
             return
         }
         if (event.key === 'Enter') {
-            const userValue = event.currentTarget.value;
-            props.AddCourse(userValue)
-            event.preventDefault();  // Stop page from refreshing
+            const userValue = event.currentTarget.value.toLowerCase();
+
+            if(validateInput(userValue))
+                props.AddCourse(userValue)
+            else
+            {
+                //alert(userValue);
+                //Notify bad entry?
+            }
+            event.preventDefault();  // Stop page from refreshing after pressing enter
             event.currentTarget.value = "";  // Clear search bar
           }
     };  
 
+    function validateInput(input : string) : boolean {
+        //Reference for valid inputs: h
+        //https://archive.registrar.ufl.edu/catalog/catalogarchive/99-00-catalog/course-numbering.html
+
+        //Check length of input
+        if(input.length < 7 || input.length > 8){
+            return false;
+        }
+        //Check for a Lab Code at the end
+        else if(input.length == 8 && (input.charAt(7) != 'c' &&  input.charAt(7) != 'l'))
+        {
+            return false;
+        }
+
+        //Check that prefix has only letters
+        var prefix = input.substring(0,3);
+        if(!(/^[a-z]+$/.test(prefix))) 
+        {
+            return false;
+        }
+        //Check that the code has 4 digits after the prefix
+        var digits = input.substring(3,7);
+        if(!(/^[0-9]+$/.test(digits))){
+            alert(digits)
+            return false;
+        }
+
+        return true;
+    }
+    
     return (
         <form action="/" method="get" className = "SearchBar">
             <input
