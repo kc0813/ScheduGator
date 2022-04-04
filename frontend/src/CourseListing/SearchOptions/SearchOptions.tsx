a2318e52f213ac4c6214aa8c18a6f558e88b42d6
pixelfac <pixelfac.nerfbros@gmail.com>
2022-04-04 18:50:10 -0400

    preserve function header standard

--- frontend/src/CourseListing/SearchOptions/SearchOptions.tsx
+++ frontend/src/CourseListing/SearchOptions/SearchOptions.tsx
@@ -1,6 +1,6 @@
 import WordCount from "./WordCount"
 
-const SearchOptions = () => {
+function SearchOptions() {
     return <div id='SearchOptions' className="searchOptions">
         Search Options
         <WordCount/>
