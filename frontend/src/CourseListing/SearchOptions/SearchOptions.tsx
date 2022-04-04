7d7ea42a17071a1a79fc485f89d25161483b1aa1
pixelfac <pixelfac.nerfbros@gmail.com>
2022-04-04 19:47:05 -0400

    move search bar into search options

--- frontend/src/CourseListing/SearchOptions/SearchOptions.tsx
+++ frontend/src/CourseListing/SearchOptions/SearchOptions.tsx
@@ -1,5 +1,5 @@
 import WordCount from "./WordCount"
-import SearchBar from "../SearchBar"
+import SearchBar from "./SearchBar"
 
 function SearchOptions(props: {AddCourse: (courseID: string) => void}) {
     return <div id='searchOptions' className="searchOptions">
