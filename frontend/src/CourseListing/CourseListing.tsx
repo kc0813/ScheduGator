11175e4481ef44793ba40870d63c38c8db6c4ada
pixelfac <pixelfac.nerfbros@gmail.com>
2022-04-04 18:46:38 -0400

    refine course listing code presentation

--- frontend/src/CourseListing/CourseListing.tsx
+++ frontend/src/CourseListing/CourseListing.tsx
@@ -2,30 +2,22 @@ import SelectedCourses from "./SelectedCourses";
 import SearchOptions from "./SearchOptions/SearchOptions";
 
 
-function CourseListing(setRenderWin : Function)
-{
+function CourseListing(setRenderWin: Function) {
     return (
         <div className="CourseListing">
             <header className="Schedule-header">
 
                 <div className="ToggleSchedule">
                     <button
-                        onClick = {() => setRenderWin("SampleSchedule")}
+                        onClick={() => setRenderWin("SampleSchedule")}
                     >
                         See Sample Schedules
-                    </button> 
-                </div>
-                
-                <div className="listCourses">
-                    List of Courses
-                    <SelectedCourses/>
+                    </button>
                 </div>
 
-                
-                <SearchOptions/>
+                <SelectedCourses />
 
-                <div className="courses">
-                </div>
+                <SearchOptions />
 
             </header>
         </div>
--- frontend/src/CourseListing/SelectedCourses.tsx
+++ frontend/src/CourseListing/SelectedCourses.tsx
@@ -6,61 +6,62 @@ import HoverBtn from './HoverButton';
 
 export function SelectedCourses() {
 
-  const [courseList, setCourseList] = useState<string[]>([])
+    const [courseList, setCourseList] = useState<string[]>([])
 
-  const deleteCourseBtn = (courseID : string) => {
-    removeCourse(courseID);
-  }
-
-  const AddCourse = (courseID : string) => {
-    if(!courseList.includes(courseID)){
-      //make copy of list
-      let tempList = courseList.slice()
-      tempList.push(courseID)
-      setCourseList(tempList)
-      console.log("ADDED: " + courseID)
-      console.log(courseList.length + " courses in List")
+    const deleteCourseBtn = (courseID: string) => {
+        removeCourse(courseID);
     }
-    else{
-      console.log("ALREADY IN")
+
+    const AddCourse = (courseID: string) => {
+        if (!courseList.includes(courseID)) {
+            //make copy of list
+            let tempList = courseList.slice()
+            tempList.push(courseID)
+            setCourseList(tempList)
+            console.log("ADDED: " + courseID)
+            console.log(courseList.length + " courses in List")
+        }
+        else {
+            console.log("ALREADY IN")
+        }
     }
-  }
 
-  const removeCourse = (courseID : string) => {
-    const index = courseList.indexOf(courseID);
-    if (index != -1) {
-      //make copy of list
-      let templist = courseList.slice()
-      templist.splice(index, 1)
-      setCourseList(templist)
-      console.log("DELETED")
-      console.log(courseList.length + " courses in List")
+    const removeCourse = (courseID: string) => {
+        const index = courseList.indexOf(courseID);
+        if (index !== -1) {
+            //make copy of list
+            let templist = courseList.slice()
+            templist.splice(index, 1)
+            setCourseList(templist)
+            console.log("DELETED")
+            console.log(courseList.length + " courses in List")
+        }
+        else {
+            console.log("Course not found to delete")
+        }
     }
-    else {
-      console.log("Course not found to delete")
-    }
-  }
 
 
-  let courseBtns : JSX.Element[] = []
-  let top = 10;
-  courseList.forEach(function(courseID : string){
-      courseBtns.push(
-        <HoverBtn courseID = {courseID} top = {top} delete = {() => deleteCourseBtn(courseID)}/>
-      )
-      top += 30;
-  });
+    let courseBtns: JSX.Element[] = []
+    let top = 10;
+    courseList.forEach(function (courseID: string) {
+        courseBtns.push(
+            <HoverBtn courseID={courseID} top={top} delete={() => deleteCourseBtn(courseID)} />
+        )
+        top += 30;
+    });
 
-  return(
-    <div id='SelCourses'>
-      <div className='inputBar'>
-        <SearchBar AddCourse={AddCourse}/>
-      </div>
-      <div id='CourseListButtons'>
-        {courseBtns}
-      </div>
-    </div>
-  );
+    return (
+        <div id='SelCourses' className='listCourses'>
+            List of Courses
+            <div className='inputBar'>
+                <SearchBar AddCourse={AddCourse} />
+            </div>
+            <div id='CourseListButtons'>
+                {courseBtns}
+            </div>
+        </div>
+    );
 }
 
 export default SelectedCourses;
