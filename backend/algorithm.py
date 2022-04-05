dfa20f689d51a94c941e0c36ad0f77a961f81404
helarious99 <helenahe@ufl.edu>
2022-04-05 19:16:25 -0400

    added a bare bones algo shell

---
+++ backend/algorithm.py
@@ -0,0 +1,20 @@
+from scheduAlgoClasses import findStaticMeetTime
+
+def buildSchedule(sections, meetDict, reservedTimes):
+    # 2D array for schedule, doesn't account for online classes
+    rows = 14
+    columns = 6
+    schedule = [[0 for i in range(columns)] for i in range(rows)]
+
+    # build static schedule
+    # add reserved timeslots - assume list of pairs w/ correct indices?
+    for i, j in reservedTimes:
+        schedule[i][j] = None
+
+    # find static meeting periods and add to schedule?
+    # check if meeting periods conflict w/ each other or reserved time slots
+     
+    # build dynamic schedule
+
+
+    return schedule
