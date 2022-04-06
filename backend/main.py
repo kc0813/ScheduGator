abba550a300103b866745d67532cb5b41bc2ad25
kc0813 <katherinechan4@gmail.com>
2022-04-05 20:52:42 -0400

    implemented getClasses and added CORS

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,22 @@
 from fastapi import Body, FastAPI
 import requests
 from models import ClassQuery
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
