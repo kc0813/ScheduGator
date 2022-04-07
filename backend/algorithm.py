dc0a0848c3db06c2240b010d016520a8b5bb9a1e
Henry Chen <henrychennj@gmail.com>
2022-04-07 01:16:54 -0400

    started implemnting static schedule building

--- backend/algorithm.py
+++ backend/algorithm.py
@@ -1,21 +1,43 @@
-from scheduAlgoClasses import findStaticMeetTime
+from scheduAlgoClasses import Schedule, Section, Course
 
-def buildSchedule(sections, meetDict, reservedTimes):
+"""
+    courses: list of all courses to be put into the schedule
+    reservedTimes: list of all time slots ["day", "period"], that should be put in the static schedule       
+"""
+def buildSchedule(courses: list, reservedTimes: list):
+
     # 2D array for schedule, doesn't account for online classes
     rows = 14
     columns = 6
-    schedule = [["" for i in range(columns)] for i in range(rows)]
-
+    schedule = {
+        "M": ["" for i in range(rows)],
+        "T": ["" for i in range(rows)],
+        "W": ["" for i in range(rows)],
+        "R": ["" for i in range(rows)],
+        "F": ["" for i in range(rows)],
+        "S": ["" for i in range(rows)],
+    }
+    template = Schedule(schedule)
+    reserved = Section("reserved", reservedTimes)
     # build static schedule
-
     # add reserved timeslots - assume list of pairs w/ correct indices?
-    for [i, j] in reservedTimes:
-        schedule[i][j] = None
+    try:
+        template.addSection(reserved)
+    except RuntimeError as err: #conflict with static times means impossible schedule
+        print(err)
+        return None
 
     # find static meeting periods and add to schedule?
-    # check if meeting periods conflict w/ each other or reserved time slots
+    for course in courses:
+        if course.staticMeetTime != []:
+            try:
+                template.addSection(course.staticMeetTime)
+            except RuntimeError as err: #conflict with static times means impossible schedule
+                print(err)
+                return None
      
     # build dynamic schedule
 
 
     return schedule
+
