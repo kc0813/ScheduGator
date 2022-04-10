eb3414a64cc69a34e7e9e5fc50ba34bf6fee6626
Henry Chen <henrychennj@gmail.com>
2022-04-10 06:01:56 -0400

    better comments for dynamic builder. Hi Nathan

--- backend/algorithm.py
+++ backend/algorithm.py
@@ -44,8 +44,10 @@ def buildSchedules(courses: list[Course], reservedTimes: list):
     # find sections that conflict with the static schedule and remove them
 
     # build dynamic schedule
-    # samples = dynamicScheduleBuilder(template, courses, 0)
-    samples = []
+    samples = dynamicScheduleBuilder(template, courses, 0)
+    if samples == []:
+        samples = None
+
     return samples
 
 
@@ -64,24 +66,30 @@ def dynamicScheduleBuilder(
     schedule: Schedule, courses: list[Course], index=0
 ) -> list[Schedule]:
 
+    """
+    Recursively runs through each section in a course at courses[index]
+    Makes a copy of the template and then tries to add a section to the copy
+    If there are more courses to consider, call this function again, but with the new copy+section
+    After making a schedule, jump back a up level and then consider another section from that course
+    """
     samples = []
     nextSchedule = Schedule()
 
-    # Run through each section in a course at courses[index]
-    # Makes a copy of the template and then tries to add to the copy
-    # If there are more courses to consider, call this function again, but with the new copy+section
     for section in courses[index].sections:
         nextSchedule = c.deepcopy(schedule)
         try:
             nextSchedule.addSection(section, courses[index].code)
-        except RuntimeError:  # what to do if there's a temporary conflict? Nothing?
-            continue
+        except RuntimeError:
+            continue  # if we run into a temp. conflict, skip this section and move on
+
+        # If it's not the last course to be added, go down a level
+        # Else, it's a complete schedule, add it to samples[]
         if index < len(courses) - 1:
+
             samples += dynamicScheduleBuilder(
                 c.deepcopy(nextSchedule), courses, index + 1
-            )
+            )  # Call the function to add the sections of the next course ("the next level down")
         else:
-            print(nextSchedule.template)
             samples.append(c.deepcopy(nextSchedule))
 
     return samples
