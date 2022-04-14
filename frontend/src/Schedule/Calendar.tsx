57f22a8e6f660e408fea3634a27b29c48a2b59c5
Henry Chen <henrychennj@gmail.com>
2022-04-14 02:13:30 -0400

    Sample schedule displays properly. Needs refactor

--- frontend/src/Schedule/Calendar.tsx
+++ frontend/src/Schedule/Calendar.tsx
@@ -1,17 +1,18 @@
 import {Table, TableBody, TableRow, TableCell} from "@material-ui/core";
 import {TableContainer, TableHead} from "@material-ui/core";
 import {Typography, Paper} from "@material-ui/core";
-import { Schedule } from "../Course";
+import { Schedule, Course } from "../Course";
 import {days, periods, TimeSlot} from "../UF";
 
 // Make sample schedule for testing rn - delete later
 let sample = new Map<string, string[]>();
-sample.set("M", ["", "", "", "", "CIS4301", "", "", "", "", "", "", "", "", ""]);
-sample.set("T", ["", "", "", "", "", "", "", "", "COP4600", "COP4600", "", "", "", ""]);
-sample.set("W", ["", "", "", "", "CIS4301", "", "", "", "", "", "", "COP4600", "", ""]);
-sample.set("R", ["", "", "", "", "", "", "", "", "", "COP4600", "", "", "", ""]);
-sample.set("F", ["", "", "", "", "CIS4301", "", "", "", "", "", "", "", "", ""]);
+sample.set("M", ["", "", "", "CIS4301", "", "", "", "", "", "", "", "", "", ""]);
+sample.set("T", ["", "", "", "", "", "", "", "COP4600", "COP4600", "", "", "", "", ""]);
+sample.set("W", ["", "", "", "CIS4301", "", "", "", "", "", "", "", "COP4600", "", ""]);
+sample.set("R", ["", "", "", "", "", "", "", "", "COP4600", "", "", "", "", ""]);
+sample.set("F", ["", "", "", "CIS4301", "", "", "", "", "", "", "", "", "", ""]);
 sample.set("S", ["", "", "", "", "", "", "", "", "", "", "", "", "", ""]);
+sample.set("ONLINE", ["EEL3872", "CAP3027"])
 
 function Calendar(
     // Reference for schedule calendar:
@@ -21,16 +22,30 @@ function Calendar(
         //schedule: Schedule
     }) {
 
+    //TODO: Clean this up and move into seperate files per component
+    let colors = ["Blue", "Chocolate", "Crimson", "DarkGreen", "SteelBlue", "MediumVioletRed", "Gold", "MediumSpringGreen"]
+    let colorMap = new Map<string, string>()
+    let i = 0;
+    let size = colors.length
     //Generate an individual cell in a row
-    const GenerateTableCell = (day:any, period:any) => {
+    const GenerateTableCell = (props: {courseID: string}) => {
+        let color: string | undefined = "white"
+        if (props.courseID != ""){
+            if(!colorMap.has(props.courseID)){
+                colorMap.set(props.courseID, colors[i%size])
+                i++;
+            }
+            color = colorMap.get(props.courseID)
+        }   
+
         return (
             <TableCell
               align="center"
               padding="none"
               scope="row" 
-              style={{width: "12%", fontSize: 12, background: "black"}}>
+              style={{width: "12%", fontSize: 12, background: color}}>
               <Typography style={{ color: "white" }}>
-                  Course ID
+                  {props.courseID}
               </Typography>
             </TableCell>
           )
@@ -46,15 +61,23 @@ function Calendar(
             <TableCell 
                 scope="row" 
                 style={{width: "12%", fontSize: 12}}>
-                {props.timeSlot.period}
+                <b>{props.timeSlot.period + ": "}</b>
+                
                 {props.timeSlot.time}
             </TableCell>
         )
-
-        calendar.forEach((value: string[], key: string) => {
+        
+        calendar.forEach((value: string[], key: string) => { 
             if (key != "ONLINE") {
-                cellArr.push(<GenerateTableCell day={key} period={props.timeSlot.period}/>)
-            }
+                let index: number
+                if(props.timeSlot.period[0] == "E"){
+                    index = parseInt(props.timeSlot.period[1]) + 10//E1 -> 11, E2 -> 12, E3 -> 13
+                }
+                else{
+                    index = parseInt(props.timeSlot.period)-1 //indicies[0-10] for sections [1-11]
+                }
+                cellArr.push(<GenerateTableCell courseID={value[index]}/>)
+            }  
         });
         
         return <TableRow>{cellArr}</TableRow>;
@@ -71,6 +94,38 @@ function Calendar(
             </TableRow>
         )
     };
+    const GenerateOnline = (props: {courseID: string}) =>{
+        let color: string | undefined
+        if(!colorMap.has(props.courseID)){
+            colorMap.set(props.courseID, colors[i%size])
+            i++;
+        }
+        color = colorMap.get(props.courseID)
+            
+        return(
+            <TableRow style={{height: "5vh"}}>
+                <TableCell
+                    style={{width: "12%", fontSize: 12}}>
+                    <b>Online</b>
+                </TableCell>
+
+                <TableCell
+                    align="center"
+                    padding="none"
+                    scope="row" 
+                    style={{width: "12%", fontSize: 12, background: color}}
+                    colSpan={6}>
+                    <Typography style={{ color: "white" }}>
+                        {props.courseID}
+                    </Typography>
+                    
+                </TableCell>
+               
+
+
+            </TableRow>)
+
+    }
 
     //Return a single sample schedule
     return(
@@ -83,16 +138,15 @@ function Calendar(
 
             <TableBody>
                 {periods.map((row) => (
-                    <GenerateRow timeSlot={row}></GenerateRow>
+                    <GenerateRow timeSlot={row}/>
                 ))}
 
-                <TableRow style={{height: "5vh"}}>
-                    <TableCell
-                        style={{width: "12%", fontSize: 12}}>
-                        Online
-                    </TableCell>
-                    <TableCell colSpan={6}/>
-                </TableRow>
+                {
+                    (sample.get("ONLINE")!).map((courseID =>(
+                        <GenerateOnline courseID={courseID}/>
+                    )))
+                }
+                
 
             </TableBody>
 
