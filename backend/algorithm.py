a823a61f3a4fbf16af33e7f278519bc252a70b8f
kc0813 <katherinechan4@gmail.com>
2022-05-21 21:48:49 -0400

    deleted algorithm and updated algorithm helper to contain all of the brute force algorithm

--- backend/algorithm.py
+++
@@ -1,7 +0,0 @@
-# from algorithmHelper import Section, Course, commonTimes
-
-
-def buildSchedule(courses):
-    """
-    Builds a schedule from a list of courses.
-    """
--- backend/algorithmHelper.py
+++ backend/algorithmHelper.py
@@ -1,5 +1,5 @@
 import re
-from typing import List
+from typing import List, Mapping, Set
 
 
 class Section:
@@ -10,6 +10,12 @@ class Section:
 
     def __init__(self, times: List[str]):
         self.times = times
+
+    def __str__(self) -> str:
+        return "[" + ", ".join(self.times) + "]"
+
+    def __repr__(self) -> str:
+        return str(self)
 
 
 class Course:
@@ -40,6 +46,86 @@ class Course:
             raise ValueError("Course code must end with l or c")
         self.code = code
 
+    def __eq__(self, __o: object) -> bool:
+        """
+        equality operator
+        """
+        return self.code == __o.code
+
+    def __repr__(self) -> str:
+        """
+        string representation
+        """
+        return self.code
+
+    def __str__(self) -> str:
+        """
+        string representation
+        """
+        return f"code: {self.code}\nsections: {self.sections}"
+
+
+class ScheduleBoard:
+    """
+    A scheduleBoard contains all possible schedules for the given courses
+    """
+
+    def __init__(self, courses: Set[Course]):
+        self.courses = courses
+        self.timeTables = self.buildBoard()
+
+    def emptyTable(self) -> Mapping[str, Mapping[str, str]]:
+        """
+        creates an empty table for the 6 days of the week and 14 periods
+        """
+        return map(
+            {
+                "M": map(),
+                "T": map(),
+                "W": map(),
+                "R": map(),
+                "F": map(),
+                "O": map(),
+            }
+        )
+
+    def addCourse(
+        self, tables: List[Mapping[str, Mapping[str, str]]], course: Course
+    ) -> None:
+        """
+        adds the course to the given tables
+        """
+        i = 0
+        for section in course.sections:
+            try:
+                table = tables[i]
+                i += 1
+            except IndexError:
+                table = self.emptyTable()
+            for time in section.times:
+                # make sure the time is not already taken,
+                # otherwise empty the table and skip to the next section
+                if time not in table or time == "o":
+                    table[time[0].upper()][time[1:]] = course.code
+                else:
+                    table = self.emptyTable()
+                    break
+            # if the table is not empty, add it to the list of potential tables
+            if table != self.emptyTable():
+                self.timeTables.append(table)
+
+    def buildBoard(self) -> List[Mapping[str, Mapping[str, str]]]:
+        """
+        builds the schedule board
+        """
+        self.timeTables = []
+        for course in self.courses:
+            self.addCourse(self.timeTables, course)
+
+    def __str__(self) -> str:
+        # TODO
+        return str(self.timeTables)
+
 
 def commonTimes(sections: List["Section"]) -> List[str]:
     """
@@ -49,3 +135,19 @@ def commonTimes(sections: List["Section"]) -> List[str]:
     for section in sections[1:]:
         times = [time for time in times if time in section.times]
     return times
+
+
+if __name__ == "__main__":
+    # test the print functions
+    section = Section(["M1", "T2", "W3", "R4", "F5", "O6"])
+    print(f"section: {section}")
+    print("---------------------------------")
+    secList = [section]
+    print(f"section list: {secList}")
+    print("---------------------------------")
+    course = Course("CAP3027", [section])
+    print(f"course: {course}")
+    print("---------------------------------")
+    couList = [course]
+    print(f"course list: {couList}")
+    print("---------------------------------")
